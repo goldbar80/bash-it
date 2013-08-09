6a38f8c8080ac9f2439c50392a2af5236c95f817
Jinha Kim <goldbar80@gmail.com>
2013-08-09 19:54:35 +0900

    modified for homebrewed rbenv

--- plugins/available/rbenv.plugin.bash
+++ plugins/available/rbenv.plugin.bash
@@ -3,7 +3,9 @@
 cite about-plugin
 about-plugin 'load rbenv, if you are using it'
 
-export PATH="$HOME/.rbenv/bin:$PATH"
+#export PATH="$HOME/.rbenv/bin:$PATH"
+export RBENV_ROOT="/usr/local/var/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
 [[ `which rbenv` ]] && eval "$(rbenv init -)"
 
 # Load the auto-completion script if rbenv was loaded.
